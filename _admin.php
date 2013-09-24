28c6b9242f0af5dde08f338467574e9ac78a7bbc
franck <carnet.franck.paul@gmail.com>
2013-09-24 13:39:33 +0200

    locales

--- _admin.php
+++ _admin.php
@@ -12,6 +12,9 @@
 
 if (!defined('DC_CONTEXT_ADMIN')) { return; }
 
+// dead but useful code, in order to have translations
+__('Comments Wikibar').__('Adds a formatting toolbar when public comments use the wiki syntax');
+
 $_menu['Blog']->addItem(__('Comments Wikibar'),'plugin.php?p=commentsWikibar','index.php?pf=commentsWikibar/icon.png',
 		preg_match('/plugin.php\?p=commentsWikibar(&.*)?$/',$_SERVER['REQUEST_URI']),
 		$core->auth->check('contentadmin',$core->blog->id));
