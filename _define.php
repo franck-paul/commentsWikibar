03848b7686d896d7a34b6ac2efa22018e70bde6b
franck <carnet.franck.paul@gmail.com>
2021-10-23 12:43:24 +0200

    Add alternative repository

--- _define.php
+++ _define.php
@@ -10,17 +10,22 @@
  * @copyright Pep
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-
-if (!defined('DC_RC_PATH')) {return;}
+if (!defined('DC_RC_PATH')) {
+    return;
+}
 
 $this->registerModule(
-    "Comments Wikibar",                                                   // Name
-    "Adds a formatting toolbar when public comments use the wiki syntax", // Description
-    "Pep, Biou, Franck Paul and contributors",                            // Author
+    'Comments Wikibar',                                                   // Name
+    'Adds a formatting toolbar when public comments use the wiki syntax', // Description
+    'Pep, Biou, Franck Paul and contributors',                            // Author
     '1.13',                                                               // Version
     [
         'requires'    => [['core', '2.17']], // Dependencies
         'permissions' => 'contentadmin',     // Permissions
-        'type'        => 'plugin'           // Type
+        'type'        => 'plugin',           // Type
+
+        'details'    => 'https://open-time.net/?q=commentsWikibar',       // Details URL
+        'support'    => 'https://github.com/franck-paul/commentsWikibar', // Support URL
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/commentsWikibar/main/dcstore.xml'
     ]
 );
