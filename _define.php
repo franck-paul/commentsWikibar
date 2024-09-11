0a349373ae91d025faa523568163b1c334c340fe
franck <carnet.franck.paul@gmail.com>
2024-09-11 09:41:39 +0200

    Code review

--- _define.php
+++ _define.php
@@ -22,6 +22,6 @@ $this->registerModule(
 
         'details'    => 'https://open-time.net/?q=commentsWikibar',
         'support'    => 'https://github.com/franck-paul/commentsWikibar',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/commentsWikibar/master/dcstore.xml',
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/commentsWikibar/main/dcstore.xml',
     ]
 );
