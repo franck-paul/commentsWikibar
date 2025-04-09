8aab87627152af302ad1068c58af39efd8d6530c
franck <carnet.franck.paul@gmail.com>
2025-04-09 16:17:46 +0200

    New release 5.8

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'Comments Wikibar',
     'Adds a formatting toolbar when public comments use the wiki syntax',
     'Pep, Biou, Franck Paul and contributors',
-    '5.7',
+    '5.8',
     [
-        'date'        => '2025-03-24T12:53:58+0100',
+        'date'        => '2025-04-09T16:17:46+0200',
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
         'type'        => 'plugin',
